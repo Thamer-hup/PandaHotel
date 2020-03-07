569566741b078c090ceaf292fd33594cd0e613cd
thamer-hub <t_h_2@yahoo.com>
2020-03-07 16:55:40 +0000

    added test and actuall classes for staff.

---
+++ Hotel Testing/Hotel Testing/tstStaff.cs
@@ -0,0 +1,22 @@
+﻿using HotelClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_Testing
+{
+    [TestClass]
+    class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStaff AStaff = new clsStaff();
+
+            Assert.IsNotNull(AStaff);
+        }
+    }
+}
